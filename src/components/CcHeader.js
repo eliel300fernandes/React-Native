a06f22b5f62b0e0b345d5731ac3697a2ce3379a2
Eliel Fernandes <39320320+eliel300fernandes@users.noreply.github.com>
2018-12-20 23:06:48 -0200

    Initial commit

---
+++ src/components/CcHeader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReacDOM from "react-dom";
+
+export default class CcHeader extends React.Component {
+  render() {
+    return <h1 class="component">Imprimindo mensagem através do componente</h1>;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CcHeader from "./components/CcHeader";
+
+import "./css/componentStyle.css";
+import "./css/defaultStyle.css";
+
+ReactDOM.render(<CcHeader />, document.getElementById("app"));
