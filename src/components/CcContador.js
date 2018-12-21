23460a6a4ee8fa22eeedb2ff9516b7a7686ecef7
Eliel Fernandes <39320320+eliel300fernandes@users.noreply.github.com>
2018-12-21 00:27:39 -0200

    Criando um contador

--- src/components/CcContador.js
+++ src/components/CcContador.js
@@ -1,16 +1,41 @@
 import React from "react";
 
 export default class CcContador extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      contador: 0
+    };
+  }
+
+  decremento() {
+    this.setState({
+      contador: this.state.contador - 1
+    });
+  }
+
+  incremento() {
+    this.setState({
+      contador: this.state.contador + 1
+    });
+  }
+
   render() {
     return (
       <div class="container">
-        <h1>Contador</h1>
+        <h1 class="text-center">Contador</h1>
         <div class="">
-          <h3 id="quant-container">0</h3>
+          <h3 id="quant-container">{this.state.contador}</h3>
 
           <div>
-            <button class="primary"> + </button>
-            <button class="danger"> - </button>
+            <button class="primary" onClick={this.incremento.bind(this)}>
+              {" "}
+              +{" "}
+            </button>
+            <button class="danger" onClick={this.decremento.bind(this)}>
+              {" "}
+              -{" "}
+            </button>
           </div>
         </div>
       </div>
